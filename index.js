253abf122a3d2900301f78e7347df957c193985a
leonardo.goandete <leonardo.goandete@hotmail.com>
2022-08-10 00:56:38 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const { request, response } = require("express");
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+const port = process.env.PORT || 3333;
+const app = express();
+app.use(express.json());
+
+const customers = [];
+
+app.get("/", (req, res) => {
+    res.send("Hello, world!");
+});
+
+app.post("/account", (request, response) => {
+    const { cpf, nome } = request.body;
+    const customersAlreadyExist = customers.some(
+      (customer) => customer.cpf === cpf
+    );
+
+    if (customersAlreadyExist){
+        return response.status(400).json({ error: "Customer already exists" });
+    }
+
+    customers.push({
+        cpf,
+        nome,
+        id: id = uuidv4(),
+        statement: [],
+    });
+
+    return response.status(201).send('<p>Cadastro do realizado com sucesso!</p>');
+});
+
+app.get("/statement", (request, response) => {
+    const { cpf } = request.headers;
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    if(!customer){
+        return response.status(400).json({ error: "Customer not found"});
+    }
+
+    return response.json(customer.statement);
+});
+
+app.listen(port, () => {
+    console.info(`Listening on http://localhost:${port}`);
+});
