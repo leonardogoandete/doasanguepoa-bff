7a9275160495c6a94816f2852d3293c552a4e973
leonardo_goandete <leonardo_goandete@sicredi.com.br>
2022-08-31 23:53:19 -0300

    Ajuste de conexão com o banco de dados

--- src/dbConnect.js
+++ src/dbConnect.js
@@ -1,19 +1,17 @@
-usuario = process.env.DB_USER;
-servidor = process.env.DB_URL;
-base = process.env.DB_DATABASE;
-porta = process.env.DB_PORT;
-const { Client } = require('pg')
+const { Client } = require('pg');
 const client = new Client({
-  user: usuario,
-  host: servidor,
-  database: base,
-  password: porta,
-  port: porta,
-  ssl: true
-})
-client.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  return response.status(201).send('<p>Banco conectado!</p>');
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
+client.connect();
+
+client.query('SELECT * FROM usuario;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
 });
