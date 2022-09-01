cc62dfd478655f4c8889c573215d41adab04f3e6
leonardo_goandete <leonardo_goandete@sicredi.com.br>
2022-09-01 00:37:02 -0300

    Iniciando implementação de crud

--- src/dbConnect.js
+++ src/dbConnect.js
@@ -7,19 +7,6 @@ const client = new Client({
 });
 
 
-/*
-client.connect();
-
-client.query('SELECT * FROM usuario;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-  
-});
-*/
-
 const getUsers = (request, response) => {
   client.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
     if (error) {
@@ -29,11 +16,59 @@ const getUsers = (request, response) => {
   })
 }
 
+const getUserById = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  client.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const createUser = (request, response) => {
+  const { name, email } = request.body
+
+  client.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).send(`User added with ID: ${results.insertId}`)
+  })
+}
+
+const updateUser = (request, response) => {
+  const id = parseInt(request.params.id)
+  const { name, email } = request.body
+
+  client.query(
+    'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+    [name, email, id],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`User modified with ID: ${id}`)
+    }
+  )
+}
+
+const deleteUser = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  client.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).send(`User deleted with ID: ${id}`)
+  })
+}
 
 module.exports = {
   getUsers,
-  //getUserById,
-  //createUser,
-  //updateUser,
-  //deleteUser,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
 }
--- src/index.js
+++ src/index.js
@@ -4,51 +4,13 @@ const app = express();
 const port = process.env.PORT || 3333;
 app.use(express.json());
 
-
-const db = require("./dbConnect.js");
-const customers = [];
-
-app.get("/", (req, res) => {
-    require("./dbConnect.js");
-});
-
-app.get('/users', db.getUsers)
-
-// middleware
-function verifyIfExistsName(request, response, next) {
-    const { name } = request.headers;
-    //const customer = customers.find(customer => customer.cpf === cpf);
-    
-    if (!customer) {
-        return response.status(400).json({ error: "Customer not found" });
-    }
-
-    request.customer = customer;
-
-    return next();
-}
-
-app.post("/account", (request, response) => {
-    const { cpf, nome } = request.body;
-    const customersAlreadyExist = customers.some(
-        (customer) => customer.cpf === cpf
-    );
-
-    if (customersAlreadyExist) {
-        return response.status(400).json({ error: "Customer already exists" });
-    }
-
-    customers.push({
-        cpf,
-        nome,
-        statement: []
-    });
-
-    return response.status(201).send('<p>Cadastro do realizado com sucesso!</p>');
-});
-
-
-
 app.listen(port, () => {
     console.info(`Listening on http://localhost:${port}`);
 });
+
+const db = require("./dbConnect.js");
+
+app.get("/users", db.getUsers)
+
+
+
