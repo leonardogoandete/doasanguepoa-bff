ac60176074cee84c9e2e9fc2a8688b82f950b9c9
leonardo_goandete <leonardo_goandete@sicredi.com.br>
2022-09-08 00:07:25 -0300

    ajuste

--- src/index.js
+++ src/index.js
@@ -1,31 +1,3 @@
-/*
-const express = require("express");
-require('dotenv').config()
-const app = express();
-const port = process.env.PORT || 3333;
-app.use(express.json());
-const db = require("./db.js");
-
-app.listen(port, () => {
-    console.info(`Listening on http://localhost:${port}`);
-});
-
-
-//Healh Check
-app.get("/", (request, response) => response.sendStatus(200));
-
-app.get('/users', db.getUsers)
-app.get('/users/:id', db.getUserById)
-app.post('/users', db.createUser)
-app.put('/users/:id', db.updateUser)
-app.delete('/users/:id', db.deleteUser)
-app.get('/instituicao', db.getInstituicao)
-app.get('/instituicao/:id', db.getInstituicaoById)
-app.post('/instituicao', db.createInstituicao)
-app.put('/instituicao/:id', db.updateInstituicao)
-app.delete('/instituicao/:id', db.deleteInstituicao)
-
-*/
 require('dotenv').config()
 const port = process.env.PORT || 3333;
 const express = require('express');
