526297dba13a836146d036721bacd920b291e2fd
leonardo_goandete <leonardo_goandete@sicredi.com.br>
2022-09-08 10:55:36 -0300

    ajuste conexão ssl postgres

--- src/db.js
+++ src/db.js
@@ -1,8 +1,4 @@
 const Sequelize = require('sequelize');
-//const sequelize = new Sequelize('postgres://postgres@localhost:5432/crud', {dialect: 'postgres'});
-
-//const sequelize = require('sequelize');
-//const db = new  sequelize(process.env.DATABASE_URL2, { dialect:'postgres' });
 const db = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     protocol: 'postgres',
