e7004671e25d9ce062b8fe01a9aeb6c4f50fb2d9
leonardo_goandete <leonardo_goandete@sicredi.com.br>
2022-09-08 10:26:46 -0300

    usndo ssl

--- src/db.js
+++ src/db.js
@@ -2,6 +2,6 @@
 //const sequelize = new Sequelize('postgres://postgres@localhost:5432/crud', {dialect: 'postgres'});
 
 const sequelize = require('sequelize');
-const db = new  sequelize(process.env.DATABASE_URL, { dialect:'postgres' });
+const db = new  sequelize(process.env.DATABASE_URL, { dialect:'postgres', ssl:'true' });
 db.sync();
 module.exports = db;
