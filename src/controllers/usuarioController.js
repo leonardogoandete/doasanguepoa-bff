32f17ff72741e69f2b78d687d4a2f68d382667bc
leonardo_goandete <leonardo_goandete@sicredi.com.br>
2022-09-23 09:54:16 -0300

    ajuste de metodo

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -82,7 +82,7 @@ const Delete = async(req, res) => {
         }
     };
 
-const Login = async(req,res) => {
+const Login = async(req,res, next) => {
 res.header("Access-Control-Allow-Origin", "*");
 res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 const {cpf, senha} = req.body;
