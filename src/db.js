c0f2625f36afa924d0dd49a85082f75c12a8c056
leonardo_goandete <leonardo_goandete@sicredi.com.br>
2022-10-20 22:12:31 -0300

    Alterado local do banco de dados

--- src/db.js
+++ src/db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize(process.env.URL_BANCO, {
+const db = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     protocol: 'postgres',
     dialectOptions: {
