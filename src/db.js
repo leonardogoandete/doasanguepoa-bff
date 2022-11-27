73cc1f424cf4447528978fd1a1bc2429a9a3d7cd
leonardogoandete <leonardogoandete>
2022-11-27 15:48:39 -0300

    ajuste de url

--- src/db.js
+++ src/db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize(process.env.DATABASE_URL, {
+const db = new Sequelize(process.env.URL_BANCO, {
     dialect: 'postgres',
     protocol: 'postgres',
     dialectOptions: {
